56fd1a63cf08f46394f7b6c1f250d514ae11291c
Samantha Gonzalez <sammyg223@csu.fullerton.edu>
2022-11-23 12:22:45 -0800

    Added two more projects

---
+++ AdApp2/AdApp2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+class AdApp2
+{
+    static void Main()
+    {
+        ClassifiedAd ad1 = new ClassifiedAd();
+        Write("What is the category of the first advertisement? ");
+        ad1.Category = ReadLine();
+        Write("How many words does it have? ");
+        ad1.Words = Convert.ToInt32(ReadLine());
+        ad1.Price = 0.09 * ad1.Words;
+
+        ClassifiedAd ad2 = new ClassifiedAd();
+        Write("What is the category of the second advertisement? ");
+        ad2.Category = ReadLine();
+        Write("How many words does it have? ");
+        ad2.Words = Convert.ToInt32(ReadLine());
+        ad2.Price = 0.09 * ad2.Words;
+
+        WriteLine("The classified ad with {0} words in category {1} costs {2:C2}", ad1.Words, ad1.Category, ad1.Price);
+        WriteLine("The classified ad with {0} words in category {1} costs {2:C2}", ad2.Words, ad2.Category, ad2.Price);
+    }
+}
+
+class ClassifiedAd //object class
+{
+    public string Category;
+    public int Words;
+    public double Price;
+}
---
+++ Advertisements/Advertisements/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Advertisements")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CSUF")]
+[assembly: AssemblyProduct("Advertisements")]
+[assembly: AssemblyCopyright("Copyright © CSUF 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0fa899b4-fd0b-41f0-abb6-b8664ce64a7c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
