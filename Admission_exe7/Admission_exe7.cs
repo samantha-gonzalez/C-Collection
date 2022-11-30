6d6cfc9483adf8dbc52d1e5ec7bd6cadea40f880
Samantha Gonzalez <sammyg223@csu.fullerton.edu>
2022-11-30 13:39:56 -0800

    more hw

---
+++ Admission_exe7/Admission_exe7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+internal class Admission_exe7
+{
+   static void Main()
+   {
+        double gpa, testScore;
+        string result;
+
+        Write("Enter your GPA >> ");
+        gpa = Convert.ToDouble(ReadLine());
+        Write("Enter your test score >> ");
+        testScore = Convert.ToDouble(ReadLine());
+
+        MakeDecision(gpa, testScore);
+        result = MakeDecision(gpa,testScore);
+        WriteLine(result);
+        
+    }
+   public static string MakeDecision(double gpa, double testScore)
+   {
+        if ((gpa >= 3.2 && testScore >= 70.00) || (gpa <= 3.2 && testScore >= 80.00))
+            return "accept";
+        else
+            return "reject"; 
+    }
+}
+
